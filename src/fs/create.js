de727ef1564ef1bb3ee6734eb9a3e4e3ed390a24
Mikita Butsko <nekete155@gmail.com>
2022-06-04 15:10:52 +0200

    feat: implement create functionality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,19 @@
+import fs from 'fs';
+
 export const create = async () => {
-    // Write your code here 
+  const fileName = 'fresh.txt';
+  const text = 'I am fresh and young';
+  const errorText = 'FS operation failed';
+
+  // Flag wx needed for error in case of file exists
+  // https://nodejs.org/api/fs.html#file-system-flags
+  return fs.writeFile(`${__dirname}/files/${fileName}`,text, { flag: 'wx'}, (err) => {
+    if (err) {
+      throw new Error(errorText);
+    }
+
+    return 1;
+  })
 };
+
+create();
