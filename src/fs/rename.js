111f817ad2a5cf850b10ee0de1d3240e23a93fb6
Mikita Butsko <nekete155@gmail.com>
2022-06-04 16:55:14 +0200

    feat: implement rename functionality

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,16 @@
+import fs from 'fs';
+import { getDirname } from '../utils/dirname.js';
+
 export const rename = async () => {
-    // Write your code here 
+  const __dirname = getDirname(import.meta.url);
+  const sourceFileName = `${__dirname}/files/wrongFilename.txt`;
+  const destFileName = `${__dirname}/files/properFilename.md`;
+
+  if (fs.existsSync(destFileName) || !fs.existsSync(sourceFileName)) {
+    throw new Error('FS operation failed');
+  }
+
+  fs.renameSync(sourceFileName, destFileName);
 };
+
+rename();
