de763afa5178194d3719b4e80b85e8ac3774309f
Mikita Butsko <nekete155@gmail.com>
2022-06-04 17:15:59 +0200

    feat: implement read functionality

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,17 @@
+import { readFile } from 'fs';
+import { getDirname } from '../utils/dirname.js';
+
 export const read = async () => {
-    // Write your code here 
+  const __dirname = getDirname(import.meta.url);
+  const sourceFileName = `${__dirname}/files/fileToRead.txt`;
+
+  readFile(sourceFileName, { encoding: 'utf8' }, (err, data) => {
+    if (err && err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+
+    console.log(data);
+  });
 };
+
+read();
