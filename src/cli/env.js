16992a6f080584b1c794b94de93687342b4d7b78
Mikita Butsko <nekete155@gmail.com>
2022-06-04 17:32:23 +0200

    feat: implement env parsing functionality

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,15 @@
+import process from 'process';
+
 export const parseEnv = () => {
-    // Write your code here 
+  const envVariables = process.env;
+
+  const rssKeys = Object.entries(envVariables)
+    .filter(([key]) => key.startsWith('RSS_'))
+    .reduce((acc, [key, value]) => {
+      return [acc, `${key}=${value}`].filter((val) => !!val).join('; ');
+    }, '');
+
+  console.log(rssKeys);
 };
+
+parseEnv();
