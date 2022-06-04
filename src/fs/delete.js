e28dcc5e7a9f7595655ed67b4007de99c04bcfcc
Mikita Butsko <nekete155@gmail.com>
2022-06-04 17:07:38 +0200

    feat: implement delete functionality

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,17 @@
+import fs from 'fs';
+import { getDirname } from '../utils/dirname.js';
+
 export const remove = async () => {
-    // Write your code here 
+  const __dirname = getDirname(import.meta.url);
+  const filename = `${__dirname}/files/fileToRemove.txt`;
+
+  fs.rm(filename, (err) => {
+    if (err && err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+
+    return 1;
+  });
 };
+
+remove();
