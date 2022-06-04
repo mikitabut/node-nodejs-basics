40fb76c2ccd130a8020e2583333903ae7d289671
Mikita Butsko <nekete155@gmail.com>
2022-06-04 15:45:33 +0200

    fix: fix dirname function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,19 +1,26 @@
 import fs from 'fs';
+import { getDirname } from '../utils/dirname.js';
 
 export const create = async () => {
+  const __dirname = getDirname(import.meta.url);
   const fileName = 'fresh.txt';
   const text = 'I am fresh and young';
   const errorText = 'FS operation failed';
 
   // Flag wx needed for error in case of file exists
   // https://nodejs.org/api/fs.html#file-system-flags
-  return fs.writeFile(`${__dirname}/files/${fileName}`,text, { flag: 'wx'}, (err) => {
-    if (err) {
-      throw new Error(errorText);
-    }
+  return fs.writeFile(
+    `${__dirname}/files/${fileName}`,
+    text,
+    { flag: 'wx' },
+    (err) => {
+      if (err.code === 'EEXIST') {
+        throw new Error(errorText);
+      }
 
-    return 1;
-  })
+      return 1;
+    }
+  );
 };
 
 create();
---
+++ src/utils/dirname.js
@@ -0,0 +1,9 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+export function getDirname(url) {
+  const __filename = fileURLToPath(url);
+  const __dirname = path.dirname(__filename);
+
+  return __dirname;
+}
