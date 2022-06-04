a0e28e87fbb3eb2a585d94fffbdce9ffdb792ea3
Mikita Butsko <nekete155@gmail.com>
2022-06-04 16:16:09 +0200

    feat: add copy folders functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import { getDirname } from '../utils/dirname.js';
+
+function copyFolderRecursive(sourceFolderName, destFolderName) {
+  const files = fs.readdirSync(sourceFolderName);
+  fs.mkdirSync(destFolderName);
+
+  for (let elName of files) {
+    const sourcePath = path.join(sourceFolderName, elName);
+    const destPath = path.join(destFolderName, elName);
+    const isDirectory = fs.lstatSync(sourcePath).isDirectory();
+
+    if (isDirectory) {
+      copyFolderRecursive(sourcePath, destPath);
+    } else {
+      fs.copyFileSync(sourcePath, destPath);
+    }
+  }
+}
+
 export const copy = async () => {
-    // Write your code here 
+  const __dirname = getDirname(import.meta.url);
+  const sourceFolderName = `${__dirname}/files`;
+  const destFolderName = `${__dirname}/files_copy`;
+
+  if (fs.existsSync(destFolderName) || !fs.existsSync(sourceFolderName)) {
+    throw new Error('FS operation failed');
+  } else {
+    copyFolderRecursive(sourceFolderName, destFolderName);
+  }
 };
+
+copy();
