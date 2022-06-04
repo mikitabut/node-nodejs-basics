ed7710df32ab71d5aa329e9426753c6b93daf8f6
Mikita Butsko <nekete155@gmail.com>
2022-06-04 18:14:23 +0200

    feat: implment args functionality

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,23 @@
+import process from 'process';
+
 export const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const parsedArgs = args.reduce((acc, el, index) => {
+    if (acc.length === 0) {
+      return el.slice(2);
+    }
+
+    if (index === 0) {
+      return acc + el.slice(2);
+    } else if (index % 2 === 0) {
+      return `${acc}, ${el.slice(2)}`;
+    } else {
+      return `${acc} is ${el}`;
+    }
+  }, '');
+
+  console.log(parsedArgs);
 };
+
+parseArgs();
