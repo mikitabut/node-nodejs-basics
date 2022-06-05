a7186456adcf0e36ae6c652b5a7fe52e914bbbdc
Mikita Butsko <nekete155@gmail.com>
2022-06-05 18:57:46 +0200

    feat: implement spawn child functionality

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,17 @@
+import { spawn } from 'child_process';
+import { getDirname } from '../utils/dirname.js';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = getDirname(import.meta.url);
+  const child = spawn(`node`, [`${__dirname}/files/script.js`, ...args]);
+
+  child.on('error', (error) => {
+    console.log(error);
+  });
+  
+  child.stdout.pipe(process.stdout);
+  process.stdin.pipe(child.stdin);
 };
+
+const args = process.argv.slice(2);
+spawnChildProcess(args);
