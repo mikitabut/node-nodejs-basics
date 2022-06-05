b696c9ba648c36d7d3de65196977d8159fe63e0d
Mikita Butsko <nekete155@gmail.com>
2022-06-05 15:26:22 +0200

    feat: implement read stream functionality

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,17 @@
+import fs from 'fs';
+import { getDirname } from '../utils/dirname.js';
+
 export const read = async () => {
-    // Write your code here 
+  const __dirname = getDirname(import.meta.url);
+  const filePath = `${__dirname}/files/fileToRead.txt`;
+
+  if (fs.existsSync(filePath)) {
+    const stream = fs.createReadStream(filePath);
+    stream.pipe(process.stdout);
+  } else {
+    // I know that this is not necessary, but why not :)
+    throw new Error('FS operation failed');
+  }
 };
+
+read();
