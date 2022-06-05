583bc56663d285dc235cafea3976e8a66e56c0c6
Mikita Butsko <nekete155@gmail.com>
2022-06-05 15:48:57 +0200

    feat: implement write stream functionality

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,18 @@
+import fs from 'fs';
+import { getDirname } from '../utils/dirname.js';
+import process from 'process';
+
 export const write = async () => {
-    // Write your code here 
+  const __dirname = getDirname(import.meta.url);
+  const filePath = `${__dirname}/files/fileToWrite.txt`;
+
+  if (fs.existsSync(filePath)) {
+    const writeStream = fs.createWriteStream(filePath);
+    process.stdin.pipe(writeStream);
+  } else {
+    // I know that this is not necessary, but why not :)
+    throw new Error('FS operation failed');
+  }
 };
+
+write();
