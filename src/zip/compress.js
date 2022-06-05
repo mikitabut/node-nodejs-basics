c1f6af20d67cc73ca491015d04535e0115e64400
Mikita Butsko <nekete155@gmail.com>
2022-06-05 17:26:17 +0200

    feat: implement zip functionality

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,22 @@
+import zlib from 'zlib';
+import fs from 'fs';
+
+import { getDirname } from '../utils/dirname.js';
+
 export const compress = async () => {
-    // Write your code here 
+  const __dirname = getDirname(import.meta.url);
+  const readFilePath = `${__dirname}/files/fileToCompress.txt`;
+  const writeFilePath = `${__dirname}/files/archive.gz`;
+  const gzip = zlib.createGzip();
+
+  if (fs.existsSync(readFilePath) && !fs.existsSync(writeFilePath)) {
+    const readStream = fs.createReadStream(readFilePath);
+    const writeStream = fs.createWriteStream(writeFilePath);
+    readStream.pipe(gzip).pipe(writeStream);
+  } else {
+    // I know that this is not necessary, but why not :)
+    throw new Error('FS operation failed');
+  }
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,22 @@
+import zlib from 'zlib';
+import fs from 'fs';
+
+import { getDirname } from '../utils/dirname.js';
+
 export const decompress = async () => {
-    // Write your code here 
+  const __dirname = getDirname(import.meta.url);
+  const readFilePath = `${__dirname}/files/archive.gz`;
+  const writeFilePath = `${__dirname}/files/fileToCompress.txt`;
+  const unzip = zlib.createUnzip();
+
+  if (fs.existsSync(readFilePath) && !fs.existsSync(writeFilePath)) {
+    const readStream = fs.createReadStream(readFilePath);
+    const writeStream = fs.createWriteStream(writeFilePath);
+    readStream.pipe(unzip).pipe(writeStream);
+  } else {
+    // I know that this is not necessary, but why not :)
+    throw new Error('FS operation failed');
+  }
 };
+
+decompress();
