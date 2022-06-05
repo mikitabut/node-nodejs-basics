f787cf85e5fcf04b9320d71be94ded191fab8c5a
Mikita Butsko <nekete155@gmail.com>
2022-06-05 17:12:45 +0200

    feat: implement trannsform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,18 @@
+import { Transform } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+  const reversedDataStream = new Transform({
+    transform(chunk, encoding, cb) {
+      let reversedData = '';
+      const data = chunk.toString('utf8');
+      for (let i = 0; i < data.length; i++) {
+        reversedData += data[data.length - 1 - i];
+      }
+      this.push(reversedData);
+      cb();
+    },
+  });
+  process.stdin.pipe(reversedDataStream).pipe(process.stdout);
 };
+
+transform();
